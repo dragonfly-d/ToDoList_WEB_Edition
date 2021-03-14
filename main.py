dbfd6e3910602f35fd1533d4b965c5964ab0f087
Vlad Boldyrev <boldyrev2040@gmail.com>
2021-03-14 22:43:42 +0300

    Добавлена возможность редактирования задачи

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, abort, request
 from data import db_session
 from data.users import User
 from data.tasks import Tasks
@@ -39,6 +39,34 @@ def add_tasks():
         db_sess.commit()
         return redirect('/tasks')
     return render_template('add_task.html', title='Adding a task', form=form)
+
+@app.route('/tasks/<int:task_id>',  methods=['GET', 'POST'])
+def edit_tasks(task_id):
+    form = TasksForm()
+
+    if request.method == "GET":
+        db_sess = db_session.create_session()
+        tasks = db_sess.query(Tasks).filter(Tasks.id == task_id).first()
+
+        if tasks:
+            form.title.data = tasks.title
+            form.done.data = tasks.done
+        else:
+            abort(404)
+
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        tasks = db_sess.query(Tasks).filter(Tasks.id == task_id).first()
+
+        if tasks:
+            tasks.title = form.title.data
+            tasks.done = form.done.data
+            db_sess.commit()
+            return redirect("/tasks")
+        else:
+            abort(404)
+            
+    return render_template('add_task.html', title='Editing a task', form=form)
 
 
 if __name__ == '__main__':
