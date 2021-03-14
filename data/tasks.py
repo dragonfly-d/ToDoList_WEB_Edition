5fe4ff2351e97a9865deaeaea93508a68a6062be
G4st3r21 <aporyadin003@gmail.com>
2021-03-14 21:12:54 +0300

    Рабочий код для DB
    Две таблицы(users, tasks)
    Код таблиц, плюс пример ввода новых  пользователей и задач

---
+++ data/tasks.py
@@ -0,0 +1,22 @@
+import datetime
+import sqlalchemy
+from sqlalchemy import orm
+from .db_session import SqlAlchemyBase
+
+
+class Tasks(SqlAlchemyBase):
+    __tablename__ = 'tasks'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+
+    title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                     default=datetime.datetime.now)
+
+    done = sqlalchemy.Column(sqlalchemy.Boolean, default=False)
+
+    user_id = sqlalchemy.Column(sqlalchemy.Integer,
+                                sqlalchemy.ForeignKey("users.id"))
+    user = orm.relation('User')
