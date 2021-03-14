5098d1773eefda0b181f47a6c8dcfeb3c4ad80c0
Vlad Boldyrev <boldyrev2040@gmail.com>
2021-03-14 22:21:45 +0300

    Добавлена возможность просмотра списка задач

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 from flask import Flask, render_template
 from data import db_session
+from data.users import User
+from data.tasks import Tasks
 
 app = Flask(__name__)
 
+def main():
+    db_session.global_init("db/TDLDataBase.db")
+    app.run(port=8080, host='127.0.0.1', debug=True)
+
 
 @app.route('/')
-def test_page():
-    return render_template('base.html', title='Test')
+def index():
+    return "Main page"
+
+@app.route("/tasks", methods=["GET"])
+def tasks():
+    db_sess = db_session.create_session()
+    tasks = db_sess.query(Tasks).all()
+
+    return render_template("index.html", title="Your Current Tasks", tasks=tasks)
 
 
 if __name__ == '__main__':
-    db_session.global_init("db/TDLDataBase.db")
-    app.run(port=8080, host='127.0.0.1', debug=True)
+    main()
