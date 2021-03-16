19901dd96056424221451c0a6f138136e44a2592
G4st3r21 <aporyadin003@gmail.com>
2021-03-16 23:02:21 +0300

    start working(not ready)

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template
 from data import db_session
+from data.users import User
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def test_page():
-    return render_template('base.html', title='Test')
+    db_sess = db_session.create_session()
+    user_name = db_sess.query(User.name).filter(User.id == 4).first()
+    return render_template('base.html', title='Test', user_name=str(user_name)[2:-3])
 
 
 if __name__ == '__main__':
